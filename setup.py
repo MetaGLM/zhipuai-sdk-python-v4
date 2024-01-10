7711c133d5db3889b30ede557c3901be0811b561
LiNY2-01 <1623482468@qq.com>
2024-01-10 10:51:41 +0800

    chore(ver): change version to v2.0.0-beta and use 'greater ' symbol for requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as requirements_file:
 
 setup(
     name="zhipuai",
-    version="v2.1.0",
+    version="v2.0.0-beta",
     description="A SDK library for accessing big model apis from ZhipuAI",
     long_description=readme,
     long_description_content_type="text/markdown",
