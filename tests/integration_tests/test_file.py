6ac2a82f6a7d7a384dd70018a7e2175e4c1a1f1c
glide-the <2533736852@qq.com>
2024-04-25 15:06:10 +0800

    CICD github action build  publish pypi、Release Tag  (#28)
    ## Add CI/CD build process, which includes the following tasks: compiling and building, releasing to test PyPI, testing with pytest, releasing to PyPI, and publishing a Release Tag.
    
    ### your configuration env to github  Repository secrets
    
    [using-secrets-in-github-actions](https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions)
    
    ### pypi 
    pypi token generate for docs [publishers](https://blog.pypi.org/posts/2023-04-20-introducing-trusted-publishers/) or [publisher](https://docs.pypi.org/trusted-publishers/adding-a-publisher/)
    #### Environment 
    - PYPI_API_TOKEN
    - TEST_PYPI_API_TOKEN
    
    ### ZhipuAI 
    ZhipuAI integration_tests for docs [bigmodel](https://open.bigmodel.cn/)
    #### Environment 
    - ZHIPUAI_API_KEY
    - ZHIPUAI_BASE_URL
    
    ### Release Tag
     
    [release-action](https://github.com/ncipollo/release-action)
    
    [managing-your-personal-access-tokens](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens)
    
    
    增加CICD构建流程，包含了如下作业，编译构建、发布test pypi、测试pytest、发布pypi、发布Release Tag
    
    需要配置仓库变量才能使用，可以在这里查看配置方法
    [using-secrets-in-github-actions](https://docs.github.com/en/actions/security-guides/using-secrets-in-github-actions)
    
    ### pypi 
    pypi token生成可以选择配置变量也可以选择pipy加入开发人员[publisher](https://docs.pypi.org/trusted-publishers/adding-a-publisher/)
    #### Environment 
    - PYPI_API_TOKEN
    - TEST_PYPI_API_TOKEN
    
    ### ZhipuAI 
    智谱的sdk配置需要从官网获取 [bigmodel](https://open.bigmodel.cn/)
     #### Environment 
    - ZHIPUAI_API_KEY
    - ZHIPUAI_BASE_URL

---
+++ tests/integration_tests/test_file.py
@@ -0,0 +1,57 @@
+from zhipuai import ZhipuAI
+import zhipuai
+import os
+
+
+def test_files(test_file_path):
+    client = ZhipuAI()
+    try:
+        result = client.files.create(
+            file=open(os.path.join(test_file_path,"demo.jsonl"), "rb"),
+            purpose="fine-tune"
+        )
+        print(result)
+        # "file-20240418025911536-6dqgr"
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
+
+
+def test_files_validation(test_file_path):
+    client = ZhipuAI()
+    try:
+        result = client.files.create(
+            file=open(os.path.join(test_file_path,"demo.jsonl"), "rb"),
+            purpose="fine-tune"
+        )
+        print(result)
+        # "file-20240418025931214-c87tj"
+
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
+
+def test_files_list():
+    client = ZhipuAI()
+    try:
+        list = client.files.list()
+        print(list)
+
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
