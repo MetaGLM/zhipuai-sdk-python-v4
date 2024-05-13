9187860b183770f61532329972f48d1aa6e76104
glide-the <2533736852@qq.com>
2024-05-13 18:27:55 +0800

    批处理对话单元测试

--- tests/integration_tests/test_batches.py
+++ tests/integration_tests/test_batches.py
@@ -14,6 +14,9 @@ def test_batch_input_file(test_file_path, logging_conf) -> None:
     )
 
     print(batch_input_file)
+
+
+#     FileObject(id='file-1w226DCBIAWg0obmdWHRXLhl', bytes=490, created_at=1715593897, filename='batchinput.jsonl', object='file', purpose='batch', status='processed', status_details=None)
 #     FileObject(id='file-MlVVOdCeGXu03QkoPOEfPdD3', bytes=490, created_at=1715395228, filename='batchinput.jsonl', object='file', purpose='batch', status='processed', status_details=None)
 
 
@@ -58,13 +61,15 @@ def test_batch_list(logging_conf) -> None:
 
     list = client.batches.list(limit=10)
     print(list)
+
+
 #     SyncCursorPage[Batch](data=[Batch(id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', completion_window='24h', created_at=1715395401, endpoint='/v1/chat/completions', input_file_id='file-MlVVOdCeGXu03QkoPOEfPdD3', object='batch', status='cancelling', cancelled_at=None, cancelling_at=1715396130, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=1715481801, failed_at=None, finalizing_at=None, in_progress_at=1715395401, metadata={'description': 'nightly eval job'}, output_file_id=None, request_counts=BatchRequestCounts(completed=2, failed=0, total=2))], object='list', first_id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', last_id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', has_more=False)
 
 
-def test_batch_result(logging_conf)->None:
+def test_batch_result(test_file_path, logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
 
-    content = client.files.content("file-xyz123")
-    with open("batchoutput.jsonl", "wb") as f:
+    content = client.files.content("file-QDpVyDIhxj8mcFiduUydNqQN")
+    with open(os.path.join(test_file_path, "batchoutput.jsonl"), "wb") as f:
         f.write(content.content)
