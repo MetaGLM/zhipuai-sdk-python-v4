abb355d5738055ab4e043ec2009ac202cc5a318c
glide-the <2533736852@qq.com>
2024-05-14 16:16:18 +0800

    批处理对话测试

--- tests/integration_tests/test_batches.py
+++ tests/integration_tests/test_batches.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import logging.config
+import zhipuai
 from zhipuai import ZhipuAI
 
 
@@ -8,72 +9,112 @@ def test_batch_input_file(test_file_path, logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
 
-    batch_input_file = client.files.create(
-        file=open(os.path.join(test_file_path, "batchinput.jsonl"), "rb"),
-        purpose="batch"
-    )
+    try:
+        batch_input_file = client.files.create(
+            file=open(os.path.join(test_file_path, "batchinput.jsonl"), "rb"),
+            purpose="batch"
+        )
 
-    print(batch_input_file)
+        print(batch_input_file)
 
+    #   FileObject(id='20240514_ea19d21b-d256-4586-b0df-e80a45e3c286', bytes=490, created_at=1715673494, filename=None, object='file', purpose='batch', status=None, status_details=None, fileName='batchinput.jsonl')
 
-#     FileObject(id='file-1w226DCBIAWg0obmdWHRXLhl', bytes=490, created_at=1715593897, filename='batchinput.jsonl', object='file', purpose='batch', status='processed', status_details=None)
-#     FileObject(id='file-MlVVOdCeGXu03QkoPOEfPdD3', bytes=490, created_at=1715395228, filename='batchinput.jsonl', object='file', purpose='batch', status='processed', status_details=None)
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
 
 
 def test_batch_create(logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        create = client.batches.create(
+            input_file_id="20240514_ea19d21b-d256-4586-b0df-e80a45e3c286",
+            endpoint="/v4/chat/completions",
+            completion_window="24h",
+            metadata={
+                "description": "job test"
+            }
+        )
+        print(create)
+        # Batch(id='batch_1790292763050508288', completion_window='24h', created_at=1715674031399, endpoint='/v4/chat/completions', input_file_id='20240514_ea19d21b-d256-4586-b0df-e80a45e3c286', object='batch', status='validating', cancelled_at=None, cancelling_at=None, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=None, failed_at=None, finalizing_at=None, in_progress_at=None, metadata={'description': 'job test'}, output_file_id=None, request_counts=BatchRequestCounts(completed=None, failed=None, total=None))
 
-    create = client.batches.create(
-        input_file_id="file-MlVVOdCeGXu03QkoPOEfPdD3",
-        endpoint="/v1/chat/completions",
-        completion_window="24h",
-        metadata={
-            "description": "nightly eval job22222"
-        }
-    )
-    print(create)
-    # Batch(id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', completion_window='24h', created_at=1715395401, endpoint='/v1/chat/completions', input_file_id='file-MlVVOdCeGXu03QkoPOEfPdD3', object='batch', status='validating', cancelled_at=None, cancelling_at=None, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=1715481801, failed_at=None, finalizing_at=None, in_progress_at=None, metadata={'description': 'nightly eval job'}, output_file_id=None, request_counts=BatchRequestCounts(completed=0, failed=0, total=0))
-    #     Batch(id='batch_eOnnpIb3yykbOc5fEg7ctc51', completion_window='24h', created_at=1715396371, endpoint='/v1/chat/completions', input_file_id='file-MlVVOdCeGXu03QkoPOEfPdD3', object='batch', status='validating', cancelled_at=None, cancelling_at=None, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=1715482771, failed_at=None, finalizing_at=None, in_progress_at=None, metadata={'description': 'nightly eval job22222'}, output_file_id=None, request_counts=BatchRequestCounts(completed=0, failed=0, total=0))
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
 
 
 def test_batch_retrieve(logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        retrieve = client.batches.retrieve("batch_1790291013237211136")
+        print(retrieve)
 
-    retrieve = client.batches.retrieve("batch_eOnnpIb3yykbOc5fEg7ctc51")
-    print(retrieve)
-    # Batch(id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', completion_window='24h', created_at=1715395401, endpoint='/v1/chat/completions', input_file_id='file-MlVVOdCeGXu03QkoPOEfPdD3', object='batch', status='in_progress', cancelled_at=None, cancelling_at=None, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=1715481801, failed_at=None, finalizing_at=None, in_progress_at=1715395401, metadata={'description': 'nightly eval job'}, output_file_id=None, request_counts=BatchRequestCounts(completed=2, failed=0, total=2))
+    #   Batch(id='batch_1790291013237211136', completion_window='24h', created_at=1715673614000, endpoint='/v4/chat/completions', input_file_id='20240514_ea19d21b-d256-4586-b0df-e80a45e3c286', object='batch', status='validating', cancelled_at=None, cancelling_at=None, completed_at=None, error_file_id='', errors=None, expired_at=None, expires_at=None, failed_at=None, finalizing_at=None, in_progress_at=None, metadata={'description': 'job test'}, output_file_id='', request_counts=BatchRequestCounts(completed=None, failed=None, total=None))
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
 
 
 def test_batch_cancel(logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
-
-    cancel = client.batches.cancel("batch_jaJkXUJJJ3212wTQ5Hd2jI1M")
-    print(cancel)
-    # Batch(id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', completion_window='24h', created_at=1715395401, endpoint='/v1/chat/completions', input_file_id='file-MlVVOdCeGXu03QkoPOEfPdD3', object='batch', status='cancelling', cancelled_at=None, cancelling_at=1715396130, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=1715481801, failed_at=None, finalizing_at=None, in_progress_at=1715395401, metadata={'description': 'nightly eval job'}, output_file_id=None, request_counts=BatchRequestCounts(completed=2, failed=0, total=2))
+    try:
+        cancel = client.batches.cancel("batch_1790291013237211136")
+        print(cancel)
+        # Batch(id='batch_1790291013237211136', completion_window='24h', created_at=1715673614000, endpoint='/v4/chat/completions', input_file_id='20240514_ea19d21b-d256-4586-b0df-e80a45e3c286', object='batch', status='cancelling', cancelled_at=None, cancelling_at=1715673698775, completed_at=None, error_file_id='', errors=None, expired_at=None, expires_at=None, failed_at=None, finalizing_at=None, in_progress_at=None, metadata={'description': 'job test'}, output_file_id='', request_counts=BatchRequestCounts(completed=None, failed=None, total=None))
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
 
 
 def test_batch_list(logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
-
-    list = client.batches.list(limit=10)
-    print(list)
-
-
-#     SyncCursorPage[Batch](data=[Batch(id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', completion_window='24h', created_at=1715395401, endpoint='/v1/chat/completions', input_file_id='file-MlVVOdCeGXu03QkoPOEfPdD3', object='batch', status='cancelling', cancelled_at=None, cancelling_at=1715396130, completed_at=None, error_file_id=None, errors=None, expired_at=None, expires_at=1715481801, failed_at=None, finalizing_at=None, in_progress_at=1715395401, metadata={'description': 'nightly eval job'}, output_file_id=None, request_counts=BatchRequestCounts(completed=2, failed=0, total=2))], object='list', first_id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', last_id='batch_jaJkXUJJJ3212wTQ5Hd2jI1M', has_more=False)
-    batch = list.get_next_page()
-    print(batch)
-    for batch in list.iter_pages():
+    try:
+        list = client.batches.list(limit=10)
+        print(list)
+        print("_________get_next_page___________")
+        batch = list.get_next_page()
         print(batch)
+        print("_________iter_pages___________")
+        for batch in list.iter_pages():
+            print(batch)
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
 
 
 def test_batch_result(test_file_path, logging_conf) -> None:
     logging.config.dictConfig(logging_conf)  # type: ignore
     client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        content = client.files.content("file-QDpVyDIhxj8mcFiduUydNqQN")
+        with open(os.path.join(test_file_path, "content_batchoutput.jsonl"), "wb") as f:
+            f.write(content.content)
+        content.write_to_file(os.path.join(test_file_path, "write_to_file_batchoutput.jsonl"))
 
-    content = client.files.content("file-QDpVyDIhxj8mcFiduUydNqQN")
-    with open(os.path.join(test_file_path, "batchoutput.jsonl"), "wb") as f:
-        f.write(content.content)
+        assert content.content == open(os.path.join(test_file_path, "batchoutput.jsonl"), "rb").read()
+        assert content.content == open(os.path.join(test_file_path, "write_to_file_batchoutput.jsonl"), "rb").read()
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
