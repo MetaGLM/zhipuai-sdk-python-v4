85cee7fbac2e3449313fbe62d6deb608a32a5962
glide-the <2533736852@qq.com>
2024-05-23 11:02:41 +0800

    定义不太文件类型解析类

--- zhipuai/core/_legacy_response.py
+++ zhipuai/core/_legacy_response.py
@@ -5,7 +5,8 @@ import inspect
 import logging
 import datetime
 import functools
-from typing import TYPE_CHECKING, Any, Union, Generic, TypeVar, Callable, Iterator, AsyncIterator, cast, overload
+from collections.abc import Iterator
+from typing import TYPE_CHECKING, Any, Union, Generic, TypeVar, Callable, cast, overload, AsyncIterator
 from typing_extensions import Awaitable, ParamSpec, override, deprecated, get_origin
 
 import anyio
@@ -18,11 +19,12 @@ from ._base_models import BaseModel, is_basemodel
 from ._constants import RAW_RESPONSE_HEADER
 from ._sse_client import StreamResponse, is_stream_class_type, extract_stream_chunk_type
 from ._errors import APIResponseValidationError
+from ._legacy_binary_response import HttpxBinaryResponseContent, HttpxTextBinaryResponseContent, HttpxResponseContent
+from ._legacy_xlsx_response import HttpxXlsxBinaryResponseContent
 
 if TYPE_CHECKING:
     from ._request_opt import FinalRequestOptions
     from ._http_client import HttpClient
-
 
 P = ParamSpec("P")
 R = TypeVar("R")
@@ -243,8 +245,17 @@ class LegacyAPIResponse(Generic[R]):
 
         origin = get_origin(cast_type) or cast_type
 
-        if inspect.isclass(origin) and issubclass(origin, HttpxBinaryResponseContent):
-            return cast(R, cast_type(response))  # type: ignore
+        if inspect.isclass(origin) and issubclass(origin, HttpxResponseContent):
+
+            # in the response, e.g. mime file
+            *_, filename = response.headers.get("content-disposition", "").split("filename=")
+            # 判断文件类型是jsonl类型的使用HttpxTextBinaryResponseContent
+            if filename and filename.endswith(".jsonl"):
+                return cast(R, HttpxTextBinaryResponseContent(response))
+            elif filename and filename.endswith(".xlsx"):
+                return cast(R, HttpxXlsxBinaryResponseContent(response))
+            else:
+                return cast(R, cast_type(response))  # type: ignore
 
         if origin == LegacyAPIResponse:
             raise RuntimeError("Unexpected state - cast_type is `APIResponse`")
@@ -337,101 +348,3 @@ def to_raw_response_wrapper(func: Callable[P, R]) -> Callable[P, LegacyAPIRespon
 
     return wrapped
 
-
-class HttpxBinaryResponseContent:
-    response: httpx.Response
-
-    def __init__(self, response: httpx.Response) -> None:
-        self.response = response
-
-    @property
-    def content(self) -> bytes:
-        return self.response.content
-
-    @property
-    def text(self) -> str:
-        return self.response.text
-
-    @property
-    def encoding(self) -> str | None:
-        return self.response.encoding
-
-    @property
-    def charset_encoding(self) -> str | None:
-        return self.response.charset_encoding
-
-    def json(self, **kwargs: Any) -> Any:
-        return self.response.json(**kwargs)
-
-    def read(self) -> bytes:
-        return self.response.read()
-
-    def iter_bytes(self, chunk_size: int | None = None) -> Iterator[bytes]:
-        return self.response.iter_bytes(chunk_size)
-
-    def iter_text(self, chunk_size: int | None = None) -> Iterator[str]:
-        return self.response.iter_text(chunk_size)
-
-    def iter_lines(self) -> Iterator[str]:
-        return self.response.iter_lines()
-
-    def iter_raw(self, chunk_size: int | None = None) -> Iterator[bytes]:
-        return self.response.iter_raw(chunk_size)
-
-    def write_to_file(
-            self,
-            file: str | os.PathLike[str],
-    ) -> None:
-        """Write the output to the given file.
-
-        Accepts a filename or any path-like object, e.g. pathlib.Path
-
-        Note: if you want to stream the data to the file instead of writing
-        all at once then you should use `.with_streaming_response` when making
-        the API request, e.g. `client.with_streaming_response.foo().stream_to_file('my_filename.txt')`
-        """
-        with open(file, mode="wb") as f:
-            for data in self.response.iter_bytes():
-                f.write(data)
-
-    def stream_to_file(
-            self,
-            file: str | os.PathLike[str],
-            *,
-            chunk_size: int | None = None,
-    ) -> None:
-        with open(file, mode="wb") as f:
-            for data in self.response.iter_bytes(chunk_size):
-                f.write(data)
-
-    def close(self) -> None:
-        return self.response.close()
-
-    async def aread(self) -> bytes:
-        return await self.response.aread()
-
-    async def aiter_bytes(self, chunk_size: int | None = None) -> AsyncIterator[bytes]:
-        return self.response.aiter_bytes(chunk_size)
-
-    async def aiter_text(self, chunk_size: int | None = None) -> AsyncIterator[str]:
-        return self.response.aiter_text(chunk_size)
-
-    async def aiter_lines(self) -> AsyncIterator[str]:
-        return self.response.aiter_lines()
-
-    async def aiter_raw(self, chunk_size: int | None = None) -> AsyncIterator[bytes]:
-        return self.response.aiter_raw(chunk_size)
-
-    async def astream_to_file(
-            self,
-            file: str | os.PathLike[str],
-            *,
-            chunk_size: int | None = None,
-    ) -> None:
-        path = anyio.Path(file)
-        async with await path.open(mode="wb") as f:
-            async for data in self.response.aiter_bytes(chunk_size):
-                await f.write(data)
-
-    async def aclose(self) -> None:
-        return await self.response.aclose()
