9a730e720d2859f055de9639f2792c0c2c33d944
jinhaiyang <haiyang.jin@aminer.cn>
2025-04-10 15:09:06 +0800

    【web_search】web_search

--- tests/integration_tests/test_videos.py
+++ tests/integration_tests/test_videos.py
@@ -1,92 +1,92 @@
-# from zhipuai import ZhipuAI
-# import zhipuai
-#
-# import logging
-# import logging.config
-#
-#
-# def test_videos(logging_conf):
-#     logging.config.dictConfig(logging_conf)  # type: ignore
-#     client = ZhipuAI()  # 填写您自己的APIKey
-#     try:
-#         response = client.videos.generations(
-#             model="cogvideo",
-#             prompt="一个开船的人",
-#
-#             user_id="1212222"
-#         )
-#         print(response)
-#
-#
-#
-#     except zhipuai.core._errors.APIRequestFailedError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIInternalError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIStatusError as err:
-#         print(err)
-#
-#
-# def test_videos_sensitive_word_check(logging_conf):
-#     logging.config.dictConfig(logging_conf)  # type: ignore
-#     client = ZhipuAI()  # 填写您自己的APIKey
-#     try:
-#         response = client.videos.generations(
-#             model="cogvideo",
-#             prompt="一个开船的人",
-#             sensitive_word_check={
-#                 "type": "ALL",
-#                 "status": "DISABLE"
-#             },
-#             user_id="1212222"
-#         )
-#         print(response)
-#
-#
-#
-#     except zhipuai.core._errors.APIRequestFailedError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIInternalError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIStatusError as err:
-#         print(err)
-#
-#
-# def test_videos_image_url(logging_conf):
-#     logging.config.dictConfig(logging_conf)  # type: ignore
-#     client = ZhipuAI()  # 填写您自己的APIKey
-#     try:
-#         response = client.videos.generations(
-#             model="cogvideo",
-#             image_url="https://cdn.bigmodel.cn/static/platform/images/solutions/car/empowerment/icon-metric.png",
-#             prompt="一些相信光的人，举着奥特曼",
-#             user_id="12222211"
-#         )
-#         print(response)
-#
-#
-#
-#     except zhipuai.core._errors.APIRequestFailedError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIInternalError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIStatusError as err:
-#         print(err)
-#
-#
-# def test_retrieve_videos_result(logging_conf):
-#     logging.config.dictConfig(logging_conf)  # type: ignore
-#     client = ZhipuAI()  # 填写您自己的APIKey
-#     try:
-#         response = client.videos.retrieve_videos_result(
-#             id="1014908869548405238276203"
-#         )
-#         print(response)
-#
-#
-#     except zhipuai.core._errors.APIRequestFailedError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIInternalError as err:
-#         print(err)
-#     except zhipuai.core._errors.APIStatusError as err:
-#         print(err)
+from zhipuai import ZhipuAI
+import zhipuai
+
+import logging
+import logging.config
+
+
+def test_videos(logging_conf):
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        response = client.videos.generations(
+            model="cogvideo",
+            prompt="一个开船的人",
+
+            user_id="1212222"
+        )
+        print(response)
+
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
+
+
+def test_videos_sensitive_word_check(logging_conf):
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        response = client.videos.generations(
+            model="cogvideo",
+            prompt="一个开船的人",
+            sensitive_word_check={
+                "type": "ALL",
+                "status": "DISABLE"
+            },
+            user_id="1212222"
+        )
+        print(response)
+
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
+
+
+def test_videos_image_url(logging_conf):
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        response = client.videos.generations(
+            model="cogvideo",
+            image_url="https://cdn.bigmodel.cn/static/platform/images/solutions/car/empowerment/icon-metric.png",
+            prompt="一些相信光的人，举着奥特曼",
+            user_id="12222211"
+        )
+        print(response)
+
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
+
+
+def test_retrieve_videos_result(logging_conf):
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+        response = client.videos.retrieve_videos_result(
+            id="1014908869548405238276203"
+        )
+        print(response)
+
+
+    except zhipuai.core._errors.APIRequestFailedError as err:
+        print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+        print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+        print(err)
