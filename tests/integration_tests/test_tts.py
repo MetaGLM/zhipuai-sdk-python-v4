533624d1e0cd7936f4f26c2f575e4fbf9c90a956
yuhongxiao <hongxiao.yu@aminer.cn>
2025-07-15 19:19:25 +0800

    Revert "feat:APIKEY"
    This reverts commit 5775bc59164c72b7df3e7485229d2ffd6f7a66f2.

--- tests/integration_tests/test_tts.py
+++ tests/integration_tests/test_tts.py
@@ -3,8 +3,8 @@ import pytest
 from zhipuai.api_resource.tts import TTSApi
 
 # 请根据实际情况填写
-BASE_URL_LIST = "https://dev.bigmodel.cn/stage-api/paas/v4"
-BASE_URL_ADD = "https://dev.bigmodel.cn/stage-api/paas/v4"
+BASE_URL_LIST = "https://dev.bigmodel.cn/stage-api/paas"
+BASE_URL_ADD = "https://dev.bigmodel.cn/stage-api/paas"
 API_KEY = "d91103df5c4a47bb808d4c84bcae9fcf.HCFkuDBhwKWLxliY"
 AUDIO_FILE_PATH = r"C:\Users\zjq18\Downloads\北京今天的天气.wav"
 
