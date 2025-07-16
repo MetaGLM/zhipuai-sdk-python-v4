0d314d2e01a1a377711391b608b98166f6ba80d3
iceAndFireisFailed <36847096+iceAndFireisFailed@users.noreply.github.com>
2025-07-16 13:43:03 +0800

    feat: tts streaming (#90)
    * 接口变更
    
    * 新增音频合成流式返回
    
    * 增加流失音频合成功能
    
    * feat:豆神一期，添加音色，查询音色列表
    
    * feat:APIKEY
    
    * feat:APIKEY
    
    * Revert "feat:APIKEY"
    
    This reverts commit 5775bc59164c72b7df3e7485229d2ffd6f7a66f2.
    
    * Revert "feat:APIKEY"
    
    This reverts commit 616b6522f55ca7e0dec2c39f30e4f3a7bb661dd1.
    
    * Revert "feat:豆神一期，添加音色，查询音色列表"
    
    This reverts commit 7d79be9d30e3bac7fb51d671c01e35a44838bf6c.
    
    * 优化
    
    * 优化测试代码
    
    * 参数优化
    
    ---------
    
    Co-authored-by: yuhongxiao <hongxiao.yu@aminer.cn>
    Co-authored-by: 赵嘉琦 <jiaqi.zhao@aminer.cn>
    Co-authored-by: tomsun28 <tomsun28@outlook.com>

---
+++ zhipuai/types/audio/audio_speech_chunk.py
@@ -0,0 +1,32 @@
+from typing import List, Optional, Dict, Any
+
+from ...core import BaseModel
+
+__all__ = [
+    "AudioSpeechChunk",
+    "AudioError",
+    "AudioSpeechChoice",
+    "AudioSpeechDelta"
+]
+
+
+class AudioSpeechDelta(BaseModel):
+    content: Optional[str] = None
+    role: Optional[str] = None
+
+
+class AudioSpeechChoice(BaseModel):
+    delta: AudioSpeechDelta
+    finish_reason: Optional[str] = None
+    index: int
+
+class AudioError:
+    code: Optional[str] = None
+    message: Optional[str] = None
+
+
+class AudioSpeechChunk(BaseModel):
+    choices: List[AudioSpeechChoice]
+    request_id: Optional[str] = None
+    created: Optional[int] = None
+    error: Optional[AudioError] = None
