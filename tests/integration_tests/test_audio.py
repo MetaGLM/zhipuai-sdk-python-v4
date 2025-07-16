c944a25f2f8368e5dfc68f03cc3ed755e1ae2023
zhaojiaqi286 <zjq18810759286@163.com>
2025-07-16 14:38:11 +0800

    Revert "feat: tts streaming (#90)"
    This reverts commit 0d314d2e01a1a377711391b608b98166f6ba80d3.

--- tests/integration_tests/test_audio.py
+++ tests/integration_tests/test_audio.py
@@ -1,5 +1,3 @@
-import base64
-import json
 import logging
 import logging.config
 from pathlib import Path
@@ -16,8 +14,7 @@ def test_audio_speech(logging_conf):
 		response = client.audio.speech(
 			model='cogtts',
 			input='你好呀,欢迎来到智谱开放平台',
-			voice='tongtong',
-			stream=False,
+			voice='female',
 			response_format='wav',
 		)
 		response.stream_to_file(speech_file_path)
@@ -28,37 +25,6 @@ def test_audio_speech(logging_conf):
 		print(err)
 	except zhipuai.core._errors.APIStatusError as err:
 		print(err)
-
-def test_audio_speech_streaming(logging_conf):
-	logging.config.dictConfig(logging_conf)  # type: ignore
-	client = ZhipuAI()  # 填写您自己的APIKey
-	try:
-		response = client.audio.speech(
-			model='cogtts',
-			input='你好呀,欢迎来到智谱开放平台',
-			voice='tongtong',
-			stream=True,
-			response_format='wav',
-		)
-		with open("output.pcm", "wb") as f:
-			for item in response:
-				choice = item.choices[0]
-				index = choice.index
-				finish_reason = choice.finish_reason
-				audio_delta = choice.delta.content
-				if finish_reason is not None:
-					break
-				f.write(base64.b64decode(audio_delta))
-				print(f"{index}.finish_reason = {finish_reason}, audio_delta = {len(audio_delta)}")
-
-	except zhipuai.core._errors.APIRequestFailedError as err:
-		print(err)
-	except zhipuai.core._errors.APIInternalError as err:
-		print(err)
-	except zhipuai.core._errors.APIStatusError as err:
-		print(err)
-	except Exception as e:
-		print(e)
 
 
 def test_audio_customization(logging_conf):
