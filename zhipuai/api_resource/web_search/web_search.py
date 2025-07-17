88a3c7c3056911cc0ccfa73bfd32f5e59221a7bf
jinhaiyang <haiyang.jin@aminer.cn>
2025-07-17 10:48:27 +0800

    search support gl and hl

--- zhipuai/api_resource/web_search/web_search.py
+++ zhipuai/api_resource/web_search/web_search.py
@@ -39,6 +39,8 @@ class WebSearchApi(BaseAPI):
             search_recency_filter: Optional[str] | NotGiven = NOT_GIVEN,
             content_size: Optional[str] | NotGiven = NOT_GIVEN,
             search_intent: Optional[bool] | NotGiven = NOT_GIVEN,
+            gl: Optional[str] | NotGiven = NOT_GIVEN,
+            hl: Optional[str] | NotGiven = NOT_GIVEN,
             extra_headers: Headers | None = None,
             extra_body: Body | None = None,
             timeout: float | httpx.Timeout | None | NotGiven = NOT_GIVEN,
@@ -56,6 +58,8 @@ class WebSearchApi(BaseAPI):
                 "search_recency_filter": search_recency_filter,
                 "content_size": content_size,
                 "search_intent": search_intent,
+                "gl": gl,
+                "hl": hl
             })
         return self._post(
             "/web_search",
