d1bf260427de42594ac47f90cbe2f747bff35a16
iceAndFireisFailed <36847096+iceAndFireisFailed@users.noreply.github.com>
2025-08-01 19:12:42 +0800

    feat: 20250801 tts mp3 (#101)
    Co-authored-by: yuhongxiao <hongxiao.yu@aminer.cn>

--- tests/integration_tests/test_audio.py
+++ tests/integration_tests/test_audio.py
@@ -30,35 +30,35 @@ def test_audio_speech(logging_conf):
 		print(err)
 
 def test_audio_speech_streaming(logging_conf):
-	logging.config.dictConfig(logging_conf)  # type: ignore
-	client = ZhipuAI()  # 填写您自己的APIKey
-	try:
-		response = client.audio.speech(
-			model='cogtts',
-			input='你好呀,欢迎来到智谱开放平台',
-			voice='tongtong',
-			stream=True,
-			response_format='wav',
-		)
-		with open("output.pcm", "wb") as f:
-			for item in response:
-				choice = item.choices[0]
-				index = choice.index
-				finish_reason = choice.finish_reason
-				audio_delta = choice.delta.content
-				if finish_reason is not None:
-					break
-				f.write(base64.b64decode(audio_delta))
-				print(f"{index}.finish_reason = {finish_reason}, audio_delta = {len(audio_delta)}")
-
-	except zhipuai.core._errors.APIRequestFailedError as err:
-		print(err)
-	except zhipuai.core._errors.APIInternalError as err:
-		print(err)
-	except zhipuai.core._errors.APIStatusError as err:
-		print(err)
-	except Exception as e:
-		print(e)
+    logging.config.dictConfig(logging_conf)  # type: ignore
+    client = ZhipuAI()  # 填写您自己的APIKey
+    try:
+       response = client.audio.speech(
+          model='cogtts',
+          input='你好呀,欢迎来到智谱开放平台',
+          voice='tongtong',
+          stream=True,
+          response_format='mp3',
+          encode_format='hex'
+       )
+       with open("output.mp3", "wb") as f:
+          for item in response:
+             choice = item.choices[0]
+             index = choice.index
+             finish_reason = choice.finish_reason
+             audio_delta = choice.delta.content
+             if finish_reason is not None:
+                break
+             f.write(bytes.fromhex(audio_delta))
+             print(f"audio delta: {audio_delta[:64]}..., 长度:{len(audio_delta)}")
+    except zhipuai.core._errors.APIRequestFailedError as err:
+       print(err)
+    except zhipuai.core._errors.APIInternalError as err:
+       print(err)
+    except zhipuai.core._errors.APIStatusError as err:
+       print(err)
+    except Exception as e:
+       print(e)
 
 
 def test_audio_customization(logging_conf):
